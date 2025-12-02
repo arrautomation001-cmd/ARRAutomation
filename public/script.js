e4ad71c2e8fb01dd7dc572771438ab68f1937b38
Arman <tumhara-main-email@example.com>
2025-12-02 16:58:21 +0530

    Update emils

--- public/script.js
+++ public/script.js
@@ -47,6 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const data = {
                 name: document.getElementById('name').value,
                 email: document.getElementById('email').value,
+                phone: document.getElementById('phone').value,
                 service: document.getElementById('service').value,
                 message: document.getElementById('message').value
             };
